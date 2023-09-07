4badbbc4b8fa7ba5ecbb4729a6c7505b075d132f
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2023-09-07 21:28:04 +0000

    chore: add docstring to Author model

--- authors/models.py
+++ authors/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Author(models.Model):
+    """
+    Book author model
+    Managed only in django admin
+    """
     name = models.CharField(max_length=200)
 
     def __str__(self):
