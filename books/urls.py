4afe5931f47fdf96ed5c9bf0c20b1138ce1f1cba
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2023-09-12 01:12:23 +0000

    chore: create a book title list view and assign a url path to it

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import book_title_list_view
+from .views import book_title_list_view, book_title_detail_view
 
 urlpatterns = [
     path("", book_title_list_view, name="book_title_list_view"),
+    path("<int:pk>/", book_title_detail_view, name="book_title_detail_view"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import BookTitle
+from .models import BookTitle, Book
 
 
 def book_title_list_view(request):
@@ -10,3 +10,12 @@ def book_title_list_view(request):
     }
     print(context['object_list'])
     return render(request, 'books/main.html', context=context)
+
+def book_title_detail_view(request, pk):
+    obj = BookTitle.objects.get(pk=pk)
+    books = obj.books
+    context = {
+        'obj': obj,
+        'books': books
+    }
+    return render(request, 'books/detail.html', context=context)
