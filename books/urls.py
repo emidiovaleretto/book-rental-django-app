1480c6b23c695f96d5070fec029055b89486c072
Emidio Valeretto <emidio.valereto@gmail.com>
2024-01-17 01:42:46 +0000

    chore: create a DetailView class and add an URL to it

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import BookTitleListView, book_title_detail_view
+from .views import BookTitleListView, BookTitleDetailView
 
 urlpatterns = [
     path("", BookTitleListView.as_view(), name="book_title_list_view"),
-    path("<int:pk>/", book_title_detail_view, name="book_title_detail_view"),
+    path("<int:pk>/", BookTitleDetailView.as_view(), name="book_title_detail_view"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import BookTitle, Book
 
 
 class BookTitleListView(ListView):
     model = BookTitle
     context_object_name = "books"
+    template_name = "books/main.html"
+    ordering = ('-created_at',)
+
+
+class BookTitleDetailView(DetailView):
+    model = BookTitle
+    context_object_name = "book"
+    template_name = "books/detail.html"
+        
     
 
-def book_title_detail_view(request, pk):
-    obj = BookTitle.objects.get(pk=pk)
-    books = obj.books
-    context = {
-        'obj': obj,
-        'books': books
-    }
-    return render(request, 'books/detail.html', context=context)
+# def book_title_detail_view(request, pk):
+#     obj = BookTitle.objects.get(pk=pk)
+#     books = obj.books
+#     context = {
+#         'obj': obj,
+#         'books': books
+#     }
+#     return render(request, 'books/detail.html', context=context)
